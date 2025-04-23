7d1e27d8c493469d3da09ac9d0e2c3d2a703f8c9
Animesh <82938725+aniasusual@users.noreply.github.com>
2025-04-23 20:20:06 +0530

    fixes

--- backend/utils/jwtToken.ts
+++ backend/utils/jwtToken.ts
@@ -1,4 +1,4 @@
-import { Response } from "express";
+import { CookieOptions, Response } from "express";
 import { IUser } from "../models/User";
 
 const sendToken = async (user: IUser, statusCode: number, res: Response): Promise<void> => {
@@ -7,13 +7,13 @@ const sendToken = async (user: IUser, statusCode: number, res: Response): Promis
     user.authToken = token;
     await user.save();
 
-    const options = {
+    const options: CookieOptions = {
         expires: new Date(
             Date.now() + Number(process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000
         ),
-        // httpOnly: true,
-        // sameSite: "None",
+        httpOnly: true,
         secure: true,
+        sameSite: 'none'
     };
 
     res.status(statusCode).cookie("token", token, options).json({
