08bafb30ba5403426cbadffd7135ba85b4349de7
Animesh <82938725+aniasusual@users.noreply.github.com>
2025-04-23 20:33:08 +0530

    some fixes for deployment

--- backend/dist/utils/jwtToken.js
+++ backend/dist/utils/jwtToken.js
@@ -16,9 +16,9 @@ const sendToken = (user, statusCode, res) => __awaiter(void 0, void 0, void 0, f
     yield user.save();
     const options = {
         expires: new Date(Date.now() + Number(process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000),
-        // httpOnly: true,
-        // sameSite: "None",
+        httpOnly: true,
         secure: true,
+        sameSite: 'none'
     };
     res.status(statusCode).cookie("token", token, options).json({
         success: true,
