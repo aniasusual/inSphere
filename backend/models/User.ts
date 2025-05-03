94c8ee64882fa109498328aa7b2c0e2f4c856993
Animesh <82938725+aniasusual@users.noreply.github.com>
2025-05-04 01:30:23 +0530

    fixes

--- backend/models/User.ts
+++ backend/models/User.ts
@@ -69,7 +69,7 @@ const userSchema: Schema<IUser> = new mongoose.Schema(
     {
         googleId: {
             type: String,
-            unique: true
+            // unique: true
         },
         firstName: {
             type: String,
